5b342dbd2910fec1415d1aa6f3dbf83a9f94e44b
Dominus_Snake <77942136+DominusSnake1@users.noreply.github.com>
2021-10-22 17:07:24 +0300

    * Created "PerceptronTraveller" Interface. * Created "Perceptron" Abstract Class. * Created 3 Perceptron Classes for each age group.

---
+++ src/gr/dit/hua/it22023/it22003/PerceptronMiddleTraveller.java
@@ -0,0 +1,15 @@
+package gr.dit.hua.it22023.it22003;
+
+import java.util.ArrayList;
+
+public class PerceptronMiddleTraveller extends Perceptron {
+
+      PerceptronMiddleTraveller(double[] Input) {
+            super(Input);
+      }
+
+      @Override
+      public ArrayList<String> recommend() {
+            return null;
+      }
+}
