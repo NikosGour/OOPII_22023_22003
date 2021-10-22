0d6e16a1ea8468347b9a50a2e71dd4cc6142ed53
NikosGour <63046380+NikosGour@users.noreply.github.com>
2021-10-22 16:35:23 +0300

    Added dummy data , and constuctor for City.java.

--- src/gr/dit/hua/it22023/it22003/City.java
+++ src/gr/dit/hua/it22023/it22003/City.java
@@ -1,6 +1,8 @@
 package gr.dit.hua.it22023.it22003;
 import gr.dit.hua.it22023.it22003.Utils;
 
+import java.util.Arrays;
+
 public class City
 {
 
@@ -19,6 +21,21 @@ public class City
      */
     private double[] features = new double[10];
     
+    /**
+     *      Marking System for Weights : Y = Young , M = Middle , E = Elder <br><hr>
+     *      index 0: Cafe                (Y M)<br>
+     *      index 1: Sea                 (Y M E)<br>
+     *      index 2: Museum              (M E)<br>
+     *      index 3: Restaurant          (Y M E)<br>
+     *      index 4: Stadium             (Y M)<br>
+     *      index 5: Landmarks           (Y M E)<br>
+     *      index 6: Hotels              (M E)<br>
+     *      index 7: Temperature         (M E)<br>
+     *      index 8: Cloudiness          (Y M)<br>
+     *      index 9: Geodesic Distance   (M E)<br>
+     */
+    private double[] normalized_features = new double[10];
+    
     //region Constants
     private static final double MIN_TERM = 0;
     private static final double MAX_TERM = 10;
@@ -29,20 +46,34 @@ public class City
     private static final double TRAVEL_AGENCY_LAT = 37.9838;
     private static final double TRAVEL_AGENCY_LON = 23.7275;
 
-    private static final int MAX_DISTANCE = 15317;
+    private static final double MAX_DISTANCE = 15325.599430089682;
     //endregion
     
-    
-    private double latitude;
-    private double longitude;
-    
-    
-    /**
-     * Same as <strong>features array</strong> but with normalized values
-     */
-    private double[] normalized_features = new double[10];
 
-
+    
+    City(double[] features , double latitude , double longitude)
+    {
+        if (features.length == 9)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                this.features[i] = features[i] > 10 ? 10 : features[i];
+            }
+            this.features[7] = features[7];
+            this.features[8] = features[8];
+            
+            this.features[9] = Utils.distance(TRAVEL_AGENCY_LAT, TRAVEL_AGENCY_LON, latitude, longitude);
+        }
+        System.out.println(Arrays.toString(this.features));
+        
+        this.normalize_features();
+    
+        System.out.println(Arrays.toString(normalized_features));
+        
+    }
+    
+   
+    
     //region Getter & Setters
     public double[] getFeatures()
     {
@@ -73,8 +104,8 @@ public class City
 
           normalized_features[7] = (this.features[7] - MIN_TEMPERATURE) / (MAX_TEMPERATURE - MIN_TEMPERATURE);
 
-          normalized_features[8] = this.features[8]/100;
+          normalized_features[8] = this.features[8] / 100;
 
-          normalized_features[9] = (Utils.distance(TRAVEL_AGENCY_LAT, TRAVEL_AGENCY_LON, this.latitude, this.longitude) / MAX_DISTANCE);
+          normalized_features[9] = this.features[9] / MAX_DISTANCE;
     }
 }
--- src/gr/dit/hua/it22023/it22003/Controler.java
+++ src/gr/dit/hua/it22023/it22003/Controler.java
@@ -1,6 +1,17 @@
-package gr.dit.hua.it22023.it22003 ;
+package gr.dit.hua.it22023.it22003;
 
-public class Controler {
-    public static void main(String[] args) {
+public class Controler
+{
+    public static void main(String[] args)
+    {
+        dummy_data();
+    }
+    
+    public static void dummy_data()
+    {
+        City athens = new City(new double[]{ 5 , 10 , 10 , 6 , 10 , 6 , 2 , 288.93 , 40 } , 37.9795 , 23.7162);
+        City rome = new City(new double[]{ 0 , 39 , 14 , 2 , 5 , 0 , 0 , 295.76 , 20 } , 41.8947 , 12.4839);
+        City new_york = new City(new double[]{2 , 52 , 29 , 7 , 12, 12 ,5, 290.37 , 1}, 40.7143 , -74.006);
+        City sydney = new City(new double[]{0, 33, 30 , 1 , 5, 5 ,2 ,289.36 , 0} , -33.8679 , 151.2073);
     }
 }
