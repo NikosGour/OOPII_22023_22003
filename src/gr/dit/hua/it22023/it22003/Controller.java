2d2d6a8a81bfd42d80a6c2f83144ff9810cb0e63
NikosGour <63046380+NikosGour@users.noreply.github.com>
2021-11-17 07:59:14 +0200

    Moved all of main methods to Utils

--- src/gr/dit/hua/it22023/it22003/Controller.java
+++ src/gr/dit/hua/it22023/it22003/Controller.java
@@ -1,28 +1,20 @@
 package gr.dit.hua.it22023.it22003;
 
-import gr.dit.hua.it22023.it22003.Models.City;
-import gr.dit.hua.it22023.it22003.Models.IncorrectArgumentException;
 import gr.dit.hua.it22023.it22003.Models.Perceptrons.Perceptron;
-import gr.dit.hua.it22023.it22003.Models.Perceptrons.PerceptronElderTraveller;
-import gr.dit.hua.it22023.it22003.Models.Perceptrons.PerceptronMiddleTraveller;
-import gr.dit.hua.it22023.it22003.Models.Perceptrons.PerceptronYoungTraveller;
 import gr.dit.hua.it22023.it22003.Utils.Utils;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Controller
 {
     public static void main(String[] args) throws IOException, InterruptedException
     {
-        program_initialization();
+        Utils.program_initialization();
     
         //noinspection InfiniteLoopStatement
         while (true)
         {
-            Perceptron Traveler = assign_age_group(read_age());
+            Perceptron Traveler = Utils.assign_age_group();
             
             for (Thread thread : Utils.threads)
             {
@@ -43,135 +35,5 @@ public class Controller
             
         }
     }
-    
-    public static void program_initialization() throws IOException
-    {
-        try
-        {
-            Utils.readJSON();
-            System.out.println("JSON read successfully");
-        } catch (FileNotFoundException e)
-        {
-            async_dummy_data();
-            System.out.println("JSON read failed");
-        }
-      
-    }
-    
-    public static void async_dummy_data()
-    {
-        Thread[] threads = {
-                        createThread("Athens" , "gr") ,
-                        createThread("Rome" , "it") ,
-                        createThread("California" , "us") ,
-                        createThread("Sydney" , "au") ,
-                        createThread("Paris" , "fr") ,
-                        createThread("London" , "uk") ,
-                        createThread("Bangkok" , "th") ,
-                        createThread("Beijing" , "cn") ,
-                        createThread("Dubai" , "uae") ,
-                        createThread("Tokyo" , "jp") ,
-                        createThread("Corfu" , "gr") };
-        
-        for (Thread thread : threads)
-        {
-            thread.start();
-        }
-        Utils.threads = new ArrayList<>(List.of(threads));
-    }
-    
-    @SuppressWarnings("unused")
-    public static void sync_dummy_data() throws IOException
-    {
-        
-        City.create_city("Athens" , "gr");
-        City.create_city("Rome" , "it");
-        City.create_city("California" , "us");
-        City.create_city("Sydney" , "au");
-        City.create_city("Paris" , "fr");
-        City.create_city("London" , "uk");
-        City.create_city("Bangkok" , "th");
-        City.create_city("Beijing" , "cn");
-        City.create_city("Dubai" , "uae");
-        City.create_city("Tokyo" , "jp");
-        City.create_city("Corfu" , "gr");
-        
-    }
-    
-    
-    public static Thread createThread(String city , String country)
-    {
-        return new Thread(() -> {
-            
-            try
-            {
-                City.create_city(city , country);
-            } catch (IOException e)
-            {
-                e.printStackTrace();
-            }
-            
-        });
-        
-    }
-    
-    public static int read_age()
-    {
-        int age = - 1;
-        System.out.println("Hello");
-        do
-        {
-            try
-            {
-                System.out.println("Please insert your age");
-                System.out.println("(Enter \"0\" if you want to EXIT the program.)");
-                System.out.print("Age : ");
-                
-                if (! Utils.scan.hasNextInt())
-                {
-                    throw new IncorrectArgumentException();
-                }
-            } catch (IncorrectArgumentException e)
-            {
-                Utils.scan.next();
-                continue;
-            }
-            age = Utils.scan.nextInt();
-            System.out.println();
-            if (age == 0)
-            {
-                try
-                {
-                    Utils.writeJSON();
-                } catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-                
-                System.out.println("Thank you for using the <<Travel-Advisor>> app!");
-                System.exit(0);
-            }
-            
-        } while ((age < 16) || (age > 115));
-        
-        return age;
-    }
-    
-    public static Perceptron assign_age_group(int age)
-    {
-        Perceptron Traveler = null;
-        
-        if (age >= 16 && age < 25)
-        {
-            Traveler = new PerceptronYoungTraveller();
-        } else if (age >= 25 && age < 60)
-        {
-            Traveler = new PerceptronMiddleTraveller();
-        } else if (age >= 60 && age < 115)
-        {
-            Traveler = new PerceptronElderTraveller();
-        }
-        
-        return Traveler;
-    }
+   
 }
--- src/gr/dit/hua/it22023/it22003/Utils/Utils.java
+++ src/gr/dit/hua/it22023/it22003/Utils/Utils.java
@@ -2,8 +2,14 @@ package gr.dit.hua.it22023.it22003.Utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gr.dit.hua.it22023.it22003.Models.City;
+import gr.dit.hua.it22023.it22003.Models.IncorrectArgumentException;
+import gr.dit.hua.it22023.it22003.Models.Perceptrons.Perceptron;
+import gr.dit.hua.it22023.it22003.Models.Perceptrons.PerceptronElderTraveller;
+import gr.dit.hua.it22023.it22003.Models.Perceptrons.PerceptronMiddleTraveller;
+import gr.dit.hua.it22023.it22003.Models.Perceptrons.PerceptronYoungTraveller;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
@@ -22,15 +28,155 @@ public final class Utils
     
     public static void writeJSON() throws IOException
     {
-        JSON_mapper.writeValue(new File("cities.json") , Utils.cities);
+        JSON_mapper.writeValue(new File("cities.json") , cities);
     }
     
     public static void readJSON() throws IOException
     {
         List<City> temp_list = Arrays.asList(JSON_mapper.readValue(new File("cities.json") , City[].class));
-        Utils.cities = new ArrayList<>(temp_list);
+        cities = new ArrayList<>(temp_list);
     }
 
+    public static void program_initialization() throws IOException
+    {
+        try
+        {
+            readJSON();
+            System.out.println("JSON read successfully");
+        } catch (FileNotFoundException e)
+        {
+            async_dummy_data();
+            System.out.println("JSON read failed");
+        }
+      
+    }
+    
+    public static void async_dummy_data()
+    {
+        Thread[] threads = {
+                        createThread("Athens" , "gr") ,
+                        createThread("Rome" , "it") ,
+                        createThread("California" , "us") ,
+                        createThread("Sydney" , "au") ,
+                        createThread("Paris" , "fr") ,
+                        createThread("London" , "uk") ,
+                        createThread("Bangkok" , "th") ,
+                        createThread("Beijing" , "cn") ,
+                        createThread("Dubai" , "uae") ,
+                        createThread("Tokyo" , "jp") ,
+                        createThread("Corfu" , "gr") };
+        
+        for (Thread thread : threads)
+        {
+            thread.start();
+        }
+        Utils.threads = new ArrayList<>(List.of(threads));
+    }
+    
+    @SuppressWarnings("unused")
+    public static void sync_dummy_data() throws IOException
+    {
+        
+        City.create_city("Athens" , "gr");
+        City.create_city("Rome" , "it");
+        City.create_city("California" , "us");
+        City.create_city("Sydney" , "au");
+        City.create_city("Paris" , "fr");
+        City.create_city("London" , "uk");
+        City.create_city("Bangkok" , "th");
+        City.create_city("Beijing" , "cn");
+        City.create_city("Dubai" , "uae");
+        City.create_city("Tokyo" , "jp");
+        City.create_city("Corfu" , "gr");
+        
+    }
+    
+    
+    public static Thread createThread(String city , String country)
+    {
+        return new Thread(() -> {
+            
+            try
+            {
+                City.create_city(city , country);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            
+        });
+        
+    }
+    
+    public static int read_age()
+    {
+        int age = - 1;
+        System.out.println("Hello");
+        do
+        {
+            try
+            {
+                System.out.println("Please insert your age");
+                System.out.println("(Enter \"0\" if you want to EXIT the program.)");
+                System.out.print("Age : ");
+                
+                if (! scan.hasNextInt())
+                {
+                    throw new IncorrectArgumentException();
+                }
+            } catch (IncorrectArgumentException e)
+            {
+                scan.next();
+                continue;
+            }
+            age = scan.nextInt();
+            System.out.println();
+            if (age == 0)
+            {
+                try
+                {
+                    writeJSON();
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                
+                System.out.println("Thank you for using the <<Travel-Advisor>> app!");
+                System.exit(0);
+            }
+            
+        } while ((age < 16) || (age > 115));
+        
+        return age;
+    }
+    
+    public static Perceptron assign_age_group()
+    {
+        int age = read_age();
+        
+        Perceptron Traveler = null;
+        
+        if (age >= 16 && age < 25)
+        {
+            Traveler = new PerceptronYoungTraveller();
+        } else if (age >= 25 && age < 60)
+        {
+            Traveler = new PerceptronMiddleTraveller();
+        } else if (age >= 60 && age < 115)
+        {
+            Traveler = new PerceptronElderTraveller();
+        }
+        
+        return Traveler;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     public static void sort_cities_by_distance()
     {
         Utils.cities.sort(Comparator.comparingDouble(o -> o.getFeatures()[9]));
