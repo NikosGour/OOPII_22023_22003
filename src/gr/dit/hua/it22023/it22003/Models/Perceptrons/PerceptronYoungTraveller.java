5f0e258ca6f939407666934649485043bb243535
Dominus_Snake <77942136+DominusSnake1@users.noreply.github.com>
2021-11-25 18:43:17 +0200

    * Added "sortRecommendations" method for YoungTraveller.

--- src/gr/dit/hua/it22023/it22003/Models/Perceptrons/PerceptronYoungTraveller.java
+++ src/gr/dit/hua/it22023/it22003/Models/Perceptrons/PerceptronYoungTraveller.java
@@ -1,6 +1,9 @@
 package gr.dit.hua.it22023.it22003.Models.Perceptrons;
 
+import gr.dit.hua.it22023.it22003.Models.City;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class PerceptronYoungTraveller extends Perceptron
 {
@@ -10,22 +13,34 @@ public class PerceptronYoungTraveller extends Perceptron
     {
         super();
     }
-    
- 
+
+    @Override
+    public ArrayList<String> sortRecommendations() {
+        this.getRecommended_cities().sort(new Comparator<>() {
+            @Override
+            public int compare(City o1, City o2) {
+                var x = o1.getFeatures()[9];
+                var y = o2.getFeatures()[9];
+
+                return Double.compare(x, y);
+            }
+        });
+        return null;
+    }
     //endregion
     
     //region Methods
-       @Override
-    public ArrayList<String> sortRecommendations()
-    {
-        return null;
-    }
-    
     @Override
     public ArrayList<String> recommend()
     {
         set_weights();
-        return super.recommend();
+        super.recommend();
+        sortRecommendations();
+
+        ArrayList<String> return_value = new ArrayList<>();
+        this.getRecommended_cities().forEach(x -> return_value.add(x.getCityName()));
+
+        return return_value;
     }
     
     
