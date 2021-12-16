3624cc755242fb102f19dbcdb6e9a00580d3f91f
NikosGour <63046380+NikosGour@users.noreply.github.com>
2021-12-16 11:17:32 +0200

    TO VRIKAAAAAAAAAAAAAA

--- src/gr/dit/hua/it22023/it22003/GUI/GUI.java
+++ src/gr/dit/hua/it22023/it22003/GUI/GUI.java
@@ -13,36 +13,28 @@ public class GUI extends JFrame
     public GUI(String title) throws HeadlessException
     {
         super(title);
-        initComponents();;
-    }
-    
-    public GUI() throws HeadlessException
-    {
-        super();
         initComponents();
-        this.setSize(new Dimension(500,500));
+        this.setPreferredSize(new Dimension(500, 500));
         this.setContentPane(Main_Panel);
         this.pack();
-        this.setLocationRelativeTo(null);
-        this.setLayout(new GridLayout());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
     }
+
     
     private void initComponents()
     {
-        button1.setSize(new Dimension(100,100));
-        button2.setSize(new Dimension(100,100));
+        button1.setSize(new Dimension(100 , 100));
+        button2.setSize(new Dimension(100 , 100));
         button1.setText("Hello");
         button2.setText("World");
-        this.add(button1);
-        this.add(button2);
+   
     }
     
     public static void CreateGUI()
     {
         GUI GUI = new GUI("Travel Advisor");
-        GUI.setVisible(true);
-
-  
+        
+        
     }
 }
