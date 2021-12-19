1021f18e88b1a7fdf2a5a99c6de78242a504f2c6
NikosGour <63046380+NikosGour@users.noreply.github.com>
2021-12-19 11:56:20 +0200

    mvn project

--- src/main/java/gr/dit/hua/it22023/it22003/Controller.java
+++ src/main/java/gr/dit/hua/it22023/it22003/Controller.java
@@ -1,6 +1,6 @@
-package main.java.gr.dit.hua.it22023.it22003;
+package gr.dit.hua.it22023.it22003;
 
-import main.java.gr.dit.hua.it22023.it22003.GUI.*;
+import gr.dit.hua.it22023.it22003.GUI.*;
 
 /**
  * Main Class with the main function
